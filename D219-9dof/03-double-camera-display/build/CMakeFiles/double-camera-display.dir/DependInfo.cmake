59fac6e6c62a3713b92424b39e2cfaae2899adc8
benson840722 <bb02880299@gamil.com>
2023-04-30 16:09:05 +0800

    C++ code test for IMX219-83

---
+++ D219-9dof/03-double-camera-display/build/CMakeFiles/double-camera-display.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/uav/code/depthmapper_testing/D219-9dof/03-double-camera-display/double-camera-display.cpp" "/home/uav/code/depthmapper_testing/D219-9dof/03-double-camera-display/build/CMakeFiles/double-camera-display.dir/double-camera-display.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ D219-9dof/05-stereoBM-camera/build/CMakeFiles/stereoBM-camera.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stereoBM-camera.dir/stereoBM-camera.cpp.o"
+  "stereoBM-camera"
+  "stereoBM-camera.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stereoBM-camera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
